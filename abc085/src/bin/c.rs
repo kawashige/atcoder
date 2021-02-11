5928a6e6abb8446dbffb3eb21cbf876bd8160173
Takahiro Kawashige <lapavoni@gmail.com>
2021-02-11 19:14:15 +0900

    Add abc085_c

--- abc085/src/bin/c.rs
+++ abc085/src/bin/c.rs
@@ -1,3 +1,25 @@
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse().ok().unwrap()
+}
 fn main() {
-    unimplemented!();
+    let (n, y) = {
+        let v = read::<String>()
+            .split_whitespace()
+            .map(|ss| ss.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+        (v[0], v[1] / 1000)
+    };
+    for i in 0..=n {
+        for j in 0..=(n - i) {
+            if 10 * i + 5 * j + n - i - j == y {
+                println!("{} {} {}", i, j, n - i - j);
+                return;
+            }
+        }
+    }
+    println!("-1 -1 -1");
 }
