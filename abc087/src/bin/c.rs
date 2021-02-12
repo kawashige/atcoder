5f040649658cf35bb05b44e96a3a430be238dcb9
Takahiro Kawashige <lapavoni@gmail.com>
2021-02-12 22:35:41 +0900

    Add abc087_c

--- abc087/src/bin/c.rs
+++ abc087/src/bin/c.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [[usize; n]; 2],
+    }
+
+    let mut sums = vec![vec![0; n + 1]; 2];
+    for i in 0..2 {
+        for j in 0..n {
+            sums[i][j + 1] = sums[i][j] + a[i][j];
+        }
+    }
+
+    let max = (0..n)
+        .map(|i| sums[0][i + 1] + sums[1][n] - sums[1][i])
+        .max()
+        .unwrap();
+    println!("{}", max);
 }
