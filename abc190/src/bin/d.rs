8ba5f3a34037d5e97591c657403701ec2593382e
Takahiro Kawashige <lapavoni@gmail.com>
2021-02-16 23:29:09 +0900

    Add abc190_d

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize
+    }
+
+    let mut sum = 3;
+    let mut count = 1;
+    let mut i = 2;
+    while sum <= n {
+        if (n - sum) % i == 0 {
+            count += 1;
+        }
+        i += 1;
+        sum += i;
+    }
+
+    println!("{}", count * 2)
 }
