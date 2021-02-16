0b242240bc8c5abc4ebaa82afc67a3f8c672f264
Takahiro Kawashige <lapavoni@gmail.com>
2021-02-16 20:43:51 +0900

    Add abc191_c

--- abc191/src/bin/c.rs
+++ abc191/src/bin/c.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        grid: [Chars; h]
+    }
+
+    let mut result = 0;
+    for i in 0..(h - 1) {
+        for j in 0..(w - 1) {
+            let mut count = 0;
+            for (x, y) in [(i, j), (i + 1, j), (i, j + 1), (i + 1, j + 1)].iter() {
+                if grid[*x][*y] == '#' {
+                    count += 1;
+                }
+            }
+            if count == 1 || count == 3 {
+                result += 1;
+            }
+        }
+    }
+    println!("{}", result);
 }
