ccc52a9580e6c992a8899026e22071b146331914
Takahiro Kawashige <lapavoni@gmail.com>
2021-02-23 20:41:16 +0900

    Add abc188_e

--- abc188/src/bin/e.rs
+++ abc188/src/bin/e.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [i32; n],
+        roads: [(usize, usize); m]
+    }
+
+    let mut lists = vec![vec![]; n];
+    for (f, t) in roads {
+        lists[f - 1].push(t - 1);
+    }
+
+    let mut min_costs = vec![None; n];
+    let mut max = std::i32::MIN;
+    for i in 0..n {
+        let mut min = a[i];
+        if let Some(v) = min_costs[i] {
+            max = std::cmp::max(max, a[i] - v);
+            min = std::cmp::min(min, v);
+        }
+        for j in &lists[i] {
+            if min_costs[*j].is_none() || Some(min) < min_costs[*j] {
+                min_costs[*j] = Some(min);
+            }
+        }
+    }
+
+    println!("{}", max);
 }
