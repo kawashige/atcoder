c42dfe1ebedd36d39162133a32c93bceb706690b
Takahiro Kawashige <lapavoni@gmail.com>
2021-03-01 21:45:09 +0900

    Add abc175_a_c

--- abc175/src/bin/a.rs
+++ abc175/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars
+    }
+
+    if s[0] == 'R' && s[1] == 'R' && s[2] == 'R' {
+        println!("3");
+    } else if (s[0] == 'R' && s[1] == 'R') || (s[1] == 'R' && s[2] == 'R') {
+        println!("2");
+    } else if s[0] == 'R' || s[1] == 'R' || s[2] == 'R' {
+        println!("1");
+    } else {
+        println!("0");
+    }
 }
--- abc175/src/bin/c.rs
+++ abc175/src/bin/c.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: i128,
+        k: i128,
+        d: i128
+    }
+
+    let x = x.abs();
+    let max = x / d;
+    if k <= max {
+        println!("{}", x - k * d);
+    } else if (k - max) % 2 == 0 {
+        println!("{}", x - max * d);
+    } else {
+        println!("{}", (x - max * d - d).abs());
+    }
 }
