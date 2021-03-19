e36f7416f93be1994d0538dd1a3c2ac8cce0249a
Takahiro Kawashige <lapavoni@gmail.com>
2021-03-19 22:30:53 +0900

    Add abc174_e

--- abc174/src/bin/e.rs
+++ abc174/src/bin/e.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        mut a: [usize; n]
+    }
+
+    a.sort_unstable();
+
+    let mut s = 0;
+    let mut e = a[n - 1];
+    while 1 < e - s {
+        let mid = (e + s) / 2;
+
+        let sum = a.iter().map(|i| ((i - 1) / mid) as u128).sum::<u128>();
+        if sum <= k as u128 {
+            e = mid;
+        } else {
+            s = mid;
+        }
+    }
+
+    println!("{}", e as u128);
 }
