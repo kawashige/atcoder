5a695c2c4f3929a9055752e6fd7029a273959675
Takahiro Kawashige <lapavoni@gmail.com>
2021-04-01 19:17:48 +0900

    Add abc145_d

--- abc145/src/bin/d.rs
+++ abc145/src/bin/d.rs
@@ -1,6 +1,7 @@
 use proconio::input;
 
-fn modinv(mut a: i64) -> i64 {
+fn modinv(a: usize) -> usize {
+    let mut a = a as i64;
     let m = 1_000_000_007;
     let mut b = m;
     let mut u = 1;
@@ -16,35 +17,39 @@ fn modinv(mut a: i64) -> i64 {
     if u < 0 {
         u += m;
     }
-    u
+    u as usize
 }
 
 fn main() {
     input! {
-        x: i64,
-        y: i64
+        x: usize,
+        y: usize
     }
-    let factorial = (1..=std::cmp::max(x, y))
-        .scan(1, |acc, i| Some(*acc * i % 1_000_000_007))
-        .collect::<Vec<i64>>();
-    println!("{:?}", factorial);
+
+    let mut factorial = vec![0; std::cmp::max(2, std::cmp::max(x, y)) as usize];
+    factorial[1] = 1;
+    for i in 2..factorial.len() {
+        factorial[i] = factorial[i - 1] * i % 1_000_000_007;
+    }
 
     let mut sum = 0;
-    for i in 0..=x {
-        if (x - i) % 2 != 0 || (x - 1) / 2 + 2 * i != y {
+
+    for i in (if x % 2 == 0 { 0 } else { 1 }..=x).step_by(2) {
+        let j = (x - i) / 2;
+
+        if i * 2 + j != y {
             continue;
         }
 
-        let j = (x - 1) / 2;
         sum = if i == 0 || j == 0 {
             sum + 1
         } else {
             (sum + factorial[(i + j) as usize] * modinv(factorial[i as usize]) % 1_000_000_007
                 * modinv(factorial[j as usize])
+                % 1_000_000_007
                 % 1_000_000_007)
                 % 1_000_000_007
         };
-        println!("i: {}, j: {}, sum: {}", i, j, sum);
     }
 
     println!("{}", sum);
