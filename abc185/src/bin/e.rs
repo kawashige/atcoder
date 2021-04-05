efc1b3f2038654e40091fe40da3b49620edcbeff
Takahiro Kawashige <lapavoni@gmail.com>
2021-04-05 22:31:26 +0900

    Add abc185_e

--- abc185/src/bin/e.rs
+++ abc185/src/bin/e.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; n],
+        b: [usize; m]
+    }
+
+    let mut dp = vec![vec![n + m; m + 1]; n + 1];
+    dp[0][0] = 0;
+    for i in 0..=n {
+        dp[i][0] = i;
+    }
+    for j in 0..=m {
+        dp[0][j] = j;
+    }
+
+    for i in 1..=n {
+        for j in 1..=m {
+            dp[i][j] = (dp[i - 1][j - 1] + if a[i - 1] != b[j - 1] { 1 } else { 0 })
+                .min(dp[i - 1][j] + 1)
+                .min(dp[i][j - 1] + 1);
+        }
+    }
+
+    println!("{}", dp[n][m]);
 }
