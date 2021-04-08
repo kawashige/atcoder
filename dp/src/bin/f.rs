3562e75aaefc56c631718d0dc9d530a3465ab2bb
Takahiro Kawashige <lapavoni@gmail.com>
2021-04-08 17:46:20 +0900

    Add dp_f

--- dp/src/bin/f.rs
+++ dp/src/bin/f.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    let mut dp = vec![vec![(0, None); t.len() + 1]; s.len() + 1];
+
+    for i in 0..s.len() {
+        for j in 0..t.len() {
+            dp[i + 1][j + 1] = if s[i] == t[j] {
+                (dp[i][j].0 + 1, Some((i, j)))
+            } else if dp[i][j + 1].0 < dp[i + 1][j].0 {
+                dp[i + 1][j]
+            } else {
+                dp[i][j + 1]
+            }
+        }
+    }
+
+    let mut prev = dp[s.len()][t.len()].1;
+    let mut chars = vec![];
+    while let Some((i, j)) = prev {
+        chars.push(s[i]);
+        prev = dp[i][j].1;
+    }
+
+    println!("{}", chars.into_iter().rev().collect::<String>());
 }
