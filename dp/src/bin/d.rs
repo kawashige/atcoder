d02ad6f9d355fcaeb72b7c1e84fbf5b9c90b4e6e
Takahiro Kawashige <lapavoni@gmail.com>
2021-04-08 11:42:09 +0900

    Add dp_d_e

--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n]
+    }
+
+    let mut dp = vec![0; w + 1];
+
+    let mut max = 0;
+    for i in 0..n {
+        for j in (0..=(w - wv[i].0)).rev() {
+            dp[j + wv[i].0] = std::cmp::max(dp[j + wv[i].0], dp[j] + wv[i].1);
+            max = std::cmp::max(max, dp[j + wv[i].0]);
+        }
+    }
+
+    println!("{}", max)
 }
--- dp/src/bin/e.rs
+++ dp/src/bin/e.rs
@@ -1,3 +1,19 @@
+use proconio::input;
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        w: u64,
+        wv: [(u64, usize); n]
+    }
+
+    let mut dp = vec![w + 1; 100001];
+    dp[0] = 0;
+
+    for i in 0..n {
+        for j in (0..(100001 - wv[i].1)).rev() {
+            dp[j + wv[i].1] = std::cmp::min(dp[j + wv[i].1], dp[j] + wv[i].0);
+        }
+    }
+
+    println!("{}", (0..100001).rev().find(|i| dp[*i] <= w).unwrap_or(0));
 }
