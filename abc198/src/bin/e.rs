a8f9a42920b04c665a63836f805f616668e401b5
Takahiro Kawashige <lapavoni@gmail.com>
2021-04-14 21:57:00 +0900

    Add abc198_e

--- abc198/src/bin/e.rs
+++ abc198/src/bin/e.rs
@@ -1,3 +1,57 @@
+use proconio::input;
+use std::collections::HashSet;
+
+fn dfs(
+    i: usize,
+    c: &Vec<usize>,
+    lists: &Vec<Vec<usize>>,
+    used: &mut Vec<bool>,
+    colors: &mut HashSet<usize>,
+    results: &mut Vec<usize>,
+) {
+    let mut is_good = false;
+    if !colors.contains(&c[i]) {
+        results.push(i + 1);
+        colors.insert(c[i]);
+        is_good = true;
+    }
+
+    for next in &lists[i] {
+        if used[*next] {
+            continue;
+        }
+        used[*next] = true;
+        dfs(*next, c, lists, used, colors, results);
+    }
+
+    if is_good {
+        colors.remove(&c[i]);
+    }
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        c: [usize; n],
+        ab: [(usize, usize); n - 1]
+    }
+
+    let mut lists = vec![vec![]; n];
+    for (a, b) in ab {
+        lists[a - 1].push(b - 1);
+        lists[b - 1].push(a - 1);
+    }
+
+    let mut used = vec![false; n];
+    used[0] = true;
+    let mut colors = HashSet::new();
+    let mut results = Vec::new();
+
+    dfs(0, &c, &&lists, &mut used, &mut colors, &mut results);
+
+    results.sort_unstable();
+
+    for i in results {
+        println!("{}", i);
+    }
 }
