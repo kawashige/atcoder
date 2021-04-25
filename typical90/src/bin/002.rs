8027c33e26808bd04df04d34c4b20ddb19e1d266
Takahiro Kawashige <lapavoni@gmail.com>
2021-04-25 15:25:58 +0900

    Add typical90_002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize
+    }
+
+    for i in 1..2_usize.pow(n as u32) {
+        let mut count = 0;
+        let mut valid = true;
+        for j in (0..n).rev() {
+            if i & 1 << j == 0 {
+                count += 1;
+            } else {
+                if count == 0 {
+                    valid = false;
+                    break;
+                } else {
+                    count -= 1;
+                }
+            }
+        }
+
+        if valid && count == 0 {
+            println!(
+                "{}",
+                (0..n)
+                    .rev()
+                    .map(|j| if i & 1 << j == 0 { '(' } else { ')' })
+                    .collect::<String>()
+            );
+        }
+    }
 }
