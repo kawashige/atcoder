b204e9d277a55b2ebf164296ed7690317f1dec15
Takahiro Kawashige <lapavoni@gmail.com>
2021-04-30 17:03:50 +0900

    Add typical90_004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h]
+    }
+
+    let mut sum_h = vec![0; h];
+    let mut sum_w = vec![0; w];
+
+    for i in 0..h {
+        for j in 0..w {
+            sum_h[i] += a[i][j];
+            sum_w[j] += a[i][j];
+        }
+    }
+
+    for i in 0..h {
+        let s = (0..w)
+            .map(|j| (sum_h[i] + sum_w[j] - a[i][j]).to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+        println!("{}", s);
+    }
 }
