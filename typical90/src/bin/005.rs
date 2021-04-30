b191b591679cef0ac7980dd6096cd50a22c49408
Takahiro Kawashige <lapavoni@gmail.com>
2021-04-30 17:04:15 +0900

    Add typical90_005(1点)

--- typical90/src/bin/005.rs
+++ typical90/src/bin/005.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+        b: usize,
+        k: usize,
+        c: [usize; k]
+    }
+
+    let mut dp = vec![vec![0; b]; 2];
+    dp[1][0] = 1;
+    let mut i = 0;
+
+    for _ in 0..n {
+        for l in 0..b {
+            for m in 0..k {
+                let remains = (l * 10 + c[m]) % b;
+                dp[i][remains] += dp[(i + 1) % 2][l];
+                dp[i][remains] %= 1_000_000_007;
+            }
+        }
+        i = (i + 1) % 2;
+        dp[i] = vec![0; b];
+    }
+
+    println!("{}", dp[(i + 1) % 2][0]);
 }
