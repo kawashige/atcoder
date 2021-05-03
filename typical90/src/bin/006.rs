dae851fc051ed0b7bc4b0e07306ade36e322add1
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-03 20:42:58 +0900

    Add typical90_006

--- typical90/src/bin/006.rs
+++ typical90/src/bin/006.rs
@@ -1,3 +1,32 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        s: Chars
+    }
+
+    let mut heap = BinaryHeap::new();
+    let mut r = String::new();
+    let mut last = 0;
+
+    for i in 0..n {
+        heap.push(Reverse((s[i], i)));
+        if k - r.len() == n - i {
+            while let Some(Reverse((c, j))) = heap.pop() {
+                if j < last {
+                    continue;
+                }
+                last = j;
+                r.push(c);
+                break;
+            }
+        }
+    }
+
+    println!("{}", r);
 }
