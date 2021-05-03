a0ec9c118a88240347186f4638a2a1d2e750bee1
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-03 18:40:33 +0900

    Update typical90_005(4点)

--- typical90/src/bin/005.rs
+++ typical90/src/bin/005.rs
@@ -1,5 +1,32 @@
 use proconio::input;
 
+fn mul(m1: &Vec<Vec<usize>>, m2: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let m = 1_000_000_007;
+    let mut r = vec![vec![0; m2[0].len()]; m1.len()];
+
+    for i in 0..m1.len() {
+        for j in 0..m2[0].len() {
+            for k in 0..m1[0].len() {
+                r[i][j] += m1[i][k] * m2[k][j];
+                r[i][j] %= m;
+            }
+        }
+    }
+
+    r
+}
+
+fn pow(m: &Vec<Vec<usize>>, n: u64) -> Vec<Vec<usize>> {
+    if n == 1 {
+        m.clone()
+    } else if n % 2 == 0 {
+        let r = pow(m, n / 2);
+        mul(&r, &r)
+    } else {
+        mul(&m, &pow(m, n - 1))
+    }
+}
+
 fn main() {
     input! {
         n: u64,
@@ -8,21 +35,15 @@ fn main() {
         c: [usize; k]
     }
 
-    let mut dp = vec![vec![0; b]; 2];
-    dp[1][0] = 1;
-    let mut i = 0;
+    let mut bb = vec![vec![0; b]; b];
 
-    for _ in 0..n {
-        for l in 0..b {
-            for m in 0..k {
-                let remains = (l * 10 + c[m]) % b;
-                dp[i][remains] += dp[(i + 1) % 2][l];
-                dp[i][remains] %= 1_000_000_007;
-            }
+    for i in 0..b {
+        for j in 0..k {
+            bb[i][(i * 10 + c[j]) % b] += 1;
         }
-        i = (i + 1) % 2;
-        dp[i] = vec![0; b];
     }
 
-    println!("{}", dp[(i + 1) % 2][0]);
+    let r = pow(&bb, n);
+
+    println!("{}", r[0][0]);
 }
