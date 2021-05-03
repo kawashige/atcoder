0067758ad280031813102378eff25bb8841dd697
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-03 15:03:04 +0900

    Add abc169_e

--- abc169/src/bin/e.rs
+++ abc169/src/bin/e.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        ab: [(usize, usize); n]
+    }
+
+    let mut mins = Vec::with_capacity(n);
+    let mut maxs = Vec::with_capacity(n);
+    for (a, b) in ab {
+        mins.push(a);
+        maxs.push(b);
+    }
+    mins.sort_unstable();
+    maxs.sort_unstable();
+
+    let median = if n % 2 == 0 {
+        maxs[n / 2] + maxs[n / 2 - 1] - mins[n / 2] - mins[n / 2 - 1] + 1
+    } else {
+        maxs[(n + 1) / 2 - 1] - mins[(n + 1) / 2 - 1] + 1
+    };
+
+    println!("{}", median);
 }
