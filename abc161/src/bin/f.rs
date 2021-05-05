8248b80339e966398fea86832d18d26a7d691b8a
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-05 11:07:48 +0900

    Add abc161_f

--- abc161/src/bin/f.rs
+++ abc161/src/bin/f.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64
+    }
+
+    let max = (n as f64).sqrt() as u64;
+    let mut factors = HashSet::new();
+
+    for i in 2..=max {
+        let mut x = n;
+        while x % i == 0 {
+            x /= i;
+        }
+        if x == 1 || (x - 1) % i == 0 {
+            factors.insert(i);
+        }
+    }
+
+    let max = ((n - 1) as f64).sqrt() as u64;
+    for i in 1..=max {
+        if (n - 1) % i == 0 {
+            factors.insert(i);
+            factors.insert((n - 1) / i);
+        }
+    }
+
+    println!("{}", factors.len());
 }
