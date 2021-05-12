5d2f3c7fe7858160fe93edf5b5156356c3e06ee8
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-12 19:54:49 +0900

    Add typical90_007_008

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        q: usize,
+        b: [usize; q]
+    }
+
+    a.sort_unstable();
+
+    for x in b {
+        let r = match a.binary_search(&x) {
+            Ok(_) => 0,
+            Err(i) => {
+                if i == 0 {
+                    a[0] - x
+                } else if i == n {
+                    x - a[n - 1]
+                } else {
+                    std::cmp::min(x - a[i - 1], a[i] - x)
+                }
+            }
+        };
+        println!("{}", r);
+    }
 }
--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,3 +1,47 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        _n: usize,
+        s: Chars
+    }
+
+    let m = 1_000_000_007;
+    let mut dp = vec![0; 7];
+
+    for c in s {
+        match c {
+            'a' => {
+                dp[0] += 1;
+            }
+            't' => {
+                dp[1] += dp[0];
+                dp[1] %= m;
+            }
+            'c' => {
+                dp[2] += dp[1];
+                dp[2] %= m;
+            }
+            'o' => {
+                dp[3] += dp[2];
+                dp[3] %= m;
+            }
+            'd' => {
+                dp[4] += dp[3];
+                dp[4] %= m;
+            }
+            'e' => {
+                dp[5] += dp[4];
+                dp[5] %= m;
+            }
+            'r' => {
+                dp[6] += dp[5];
+                dp[6] %= m;
+            }
+            _ => {}
+        }
+    }
+
+    println!("{}", dp[6]);
 }
