2ecb4661d1c4a668fda0f0179ee9a6a76424d8f4
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-16 17:36:47 +0900

    Add abc201_e

--- abc201/src/bin/e.rs
+++ abc201/src/bin/e.rs
@@ -1,32 +1,54 @@
 use proconio::input;
-
-fn dfs(node: usize, list: &Vec<Vec<usize>>, seen: &mut Vec<bool>, counts: &mut Vec<usize>) {
-    let mut count = 0;
-    seen[node] = true;
-    for next in &list[node] {
-        if seen[*next] {
-            continue;
-        }
-        dfs(*next, list, seen, counts);
-        count += counts[*next] + 1;
-    }
-    counts[node] = count;
-}
+use std::collections::VecDeque;
 
 fn main() {
     input! {
         n: usize,
-        uvw: [(usize, usize, usize); n - 1]
+        uvw: [(usize, usize, u64); n - 1]
     }
     let mut list = vec![vec![]; n];
-    for (u, v, _) in &uvw {
-        list[u - 1].push(v - 1);
-        list[v - 1].push(u - 1);
+    for (u, v, w) in uvw {
+        list[u - 1].push((v - 1, w));
+        list[v - 1].push((u - 1, w));
     }
 
     let mut seen = vec![false; n];
-    let mut counts = vec![0; n];
-    dfs(0, &list, &mut seen, &mut counts);
+    let mut nums = Vec::with_capacity(n - 1);
+    let mut queue = VecDeque::new();
 
-    // println!("{:?}", counts);
+    for (next, weight) in &list[0] {
+        queue.push_back((*next, *weight))
+    }
+
+    while let Some((node, weight)) = queue.pop_back() {
+        nums.push(weight);
+        seen[node] = true;
+
+        for (n, w) in &list[node] {
+            if seen[*n] {
+                continue;
+            }
+            queue.push_back((*n, w ^ weight));
+        }
+    }
+
+    let m = 1_000_000_007;
+    let mut count_zero = [0_u64; 64];
+    let mut count_one = [0_u64; 64];
+
+    let mut sum = 0;
+    for i in (0..n).rev() {
+        let mut mul = 1;
+        for j in 0..64 {
+            if nums[i] & 1 << j == 0 {
+                sum = (sum + count_one[j] * mul % m) % m;
+                count_zero[j] += 1;
+            } else {
+                sum = (sum + count_zero[j] * mul % m) % m;
+                count_one[j] += 1;
+            }
+            mul = (mul * 2) % m;
+        }
+    }
+    println!("{}", sum);
 }
