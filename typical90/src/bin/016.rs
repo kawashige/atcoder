2c6c539190fbfdda70100e6df3e0a2dc3c5d025a
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-20 21:48:17 +0900

    Add typical90_016_018

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        c: usize
+    }
+
+    let mut min = 10000;
+    for i in 0..10000 {
+        let c_a = i * a;
+        if c_a > n {
+            break;
+        }
+        for j in 0..(10000 - i) {
+            let sum = c_a + j * b;
+            if sum > n {
+                break;
+            }
+            if (n - sum) % c == 0 {
+                min = std::cmp::min(min, i + j + (n - sum) / c);
+            }
+        }
+    }
+
+    println!("{}", min);
 }
--- typical90/src/bin/018.rs
+++ typical90/src/bin/018.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        t: f64,
+        l: f64,
+        x: f64,
+        y: f64,
+        q: usize,
+        ee: [f64; q]
+    }
+
+    for e in ee {
+        let theta = (360.0 * e) / t;
+
+        let rad = theta * std::f64::consts::PI / 180.0;
+        let p = (
+            0.0,
+            rad.sin() * (-l / 2.0),
+            rad.cos() * (-l / 2.0) + l / 2.0,
+        );
+
+        let xy = (x, y - p.1, 0.0);
+
+        let r = (p.2 / (xy.0 * xy.0 + xy.1 * xy.1).sqrt()).atan() * 180.0 / std::f64::consts::PI;
+        println!("{}", r);
+    }
 }
