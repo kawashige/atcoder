ab98364757d44ab5d83eb6f05c91d3c2a286ea8f
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-24 20:27:52 +0900

    Add typical90_020

--- typical90/src/bin/020.rs
+++ typical90/src/bin/020.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: u64,
+        mut b: u64,
+        mut c: u64
+    }
+
+    let mut num = 1;
+    while b > 0 {
+        num *= c;
+        b -= 1;
+    }
+
+    if a < num {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
