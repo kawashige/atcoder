80f660e11da8b0f80bec285d19410c00b635e1d9
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-28 21:04:40 +0900

    Add abc086_b

--- abc086/src/bin/b.rs
+++ abc086/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize
+    }
+
+    let num = format!("{}{}", a, b).parse::<usize>().unwrap();
+    for i in 1..=((num as f64).sqrt() as usize) {
+        if i * i == num {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
