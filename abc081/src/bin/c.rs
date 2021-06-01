ae2ccf535dcaaf9aa4dd84410f86b0db63c21b14
Takahiro Kawashige <lapavoni@gmail.com>
2021-06-01 19:41:44 +0900

    Add abc081_c_d

--- abc081/src/bin/c.rs
+++ abc081/src/bin/c.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n]
+    }
+
+    let mut map = HashMap::new();
+    for x in a {
+        *map.entry(x).or_insert(0) += 1;
+    }
+    let mut counts = map.into_iter().map(|(_, v)| v).collect::<Vec<usize>>();
+
+    if counts.len() <= k {
+        println!("0");
+        return;
+    }
+
+    counts.sort_unstable();
+    let l = counts.len();
+    let sum = counts.into_iter().take(l - k).sum::<usize>();
+
+    println!("{}", sum);
 }
--- abc081/src/bin/d.rs
+++ abc081/src/bin/d.rs
@@ -1,3 +1,62 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [i32; n]
+    }
+
+    let min_i = (0..n).min_by_key(|i| a[*i]).unwrap();
+    let max_i = (0..n).max_by_key(|i| a[*i]).unwrap();
+
+    let mut results = Vec::new();
+
+    if a[min_i] < 0 && 0 < a[max_i] {
+        if a[min_i].abs() < a[max_i] {
+            for i in 0..n {
+                if i != max_i && a[i] < 0 {
+                    a[i] += a[max_i];
+                    results.push((max_i, i));
+                }
+            }
+            let mut target = max_i;
+            for i in 0..n {
+                a[i] += a[target];
+                results.push((target, i));
+                target = i;
+            }
+        } else {
+            for i in 0..n {
+                if i != min_i && a[i] > 0 {
+                    a[i] += a[min_i];
+                    results.push((min_i, i));
+                }
+            }
+            let mut target = min_i;
+            for i in (0..n).rev() {
+                a[i] += a[target];
+                results.push((target, i));
+                target = i;
+            }
+        };
+    } else if a[max_i] > 0 {
+        let mut target = max_i;
+        for i in 0..n {
+            a[i] += a[target];
+            results.push((target, i));
+            target = i;
+        }
+    } else if a[min_i] < 0 {
+        let mut target = min_i;
+        for i in (0..n).rev() {
+            a[i] += a[target];
+            results.push((target, i));
+            target = i;
+        }
+    }
+
+    println!("{}", results.len());
+    for (i, j) in results {
+        println!("{} {}", i + 1, j + 1);
+    }
 }
