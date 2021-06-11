251a3a2cf831731a67034fed554d8170c6089274
Takahiro Kawashige <lapavoni@gmail.com>
2021-06-11 18:48:10 +0900

    Add abc131_e

--- abc131/src/bin/e.rs
+++ abc131/src/bin/e.rs
@@ -1,3 +1,40 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize
+    }
+
+    if k > (n - 1) * (n - 2) / 2 {
+        println!("-1");
+        return;
+    }
+
+    let mut m = n - 1;
+
+    let mut edges = Vec::new();
+    for i in 2..=n {
+        edges.push((1, i))
+    }
+
+    let mut remains = (n - 1) * (n - 2) / 2 - k;
+    for i in 2..=n {
+        if remains == 0 {
+            break;
+        }
+        for j in (i + 1)..=n {
+            edges.push((i, j));
+            remains -= 1;
+            m += 1;
+            if remains == 0 {
+                break;
+            }
+        }
+    }
+
+    println!("{}", m);
+    for (f, t) in edges {
+        println!("{} {}", f, t);
+    }
 }
