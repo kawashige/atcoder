90480698958596411a7129610c5593026f567506
Takahiro Kawashige <lapavoni@gmail.com>
2021-06-14 21:26:11 +0900

    Add arc120_b

--- arc120/src/bin/b.rs
+++ arc120/src/bin/b.rs
@@ -7,4 +7,44 @@ fn main() {
         w: usize,
         s: [Chars; h]
     }
+
+    let m = 998244353;
+    let mut r = 1;
+
+    for i in 0..h {
+        let mut count = vec![0; 3];
+
+        for j in 0..=std::cmp::min(i, w - 1) {
+            match s[i - j][j] {
+                'R' => count[0] += 1,
+                'B' => count[1] += 1,
+                _ => count[2] += 1,
+            }
+        }
+        if count[0] > 0 && count[1] > 0 {
+            println!("0");
+            return;
+        } else if count[0] == 0 && count[1] == 0 {
+            r = r * 2 % m;
+        }
+    }
+    for i in 1..w {
+        let mut count = vec![0; 3];
+
+        for j in 0..=std::cmp::min(h - 1, w - 1 - i) {
+            match s[h - 1 - j][i + j] {
+                'R' => count[0] += 1,
+                'B' => count[1] += 1,
+                _ => count[2] += 1,
+            }
+        }
+        if count[0] > 0 && count[1] > 0 {
+            println!("0");
+            return;
+        } else if count[0] == 0 && count[1] == 0 {
+            r = r * 2 % m;
+        }
+    }
+
+    println!("{}", r);
 }
