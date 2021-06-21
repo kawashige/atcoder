93c54eab34cb13d1287ef8cb9b893fcf7cd88746
Takahiro Kawashige <lapavoni@gmail.com>
2021-06-21 20:05:21 +0900

    Add abc206_e(解説AC)

--- abc206/src/bin/e.rs
+++ abc206/src/bin/e.rs
@@ -6,41 +6,20 @@ fn main() {
         r: usize
     }
 
-    let mut primes = vec![true; r + 1];
-    primes[0] = false;
-    primes[1] = false;
-    for i in 2..=((r as f64).sqrt() as usize) {
-        if !primes[i] {
-            continue;
-        }
-
-        for j in ((i + i)..primes.len()).step_by(i) {
-            primes[j] = false;
-        }
-    }
-
-    let mut spf = (0..=r).collect::<Vec<usize>>();
-    for i in 2..=r {
-        if spf[i] == i {
-            for j in ((i * 2)..=r).step_by(i) {
-                spf[j] = i;
-            }
-        }
-    }
-    // println!("primes: {:?}", primes);
-    // println!("spf: {:?}", spf);
-
     let mut count = 0;
-    for i in 2..=(r / 2) {
-        if primes[i] {
-            let x = (r / i) - (if i < l { l - 1 } else { i }) / i;
-            count += (x - 1) * x;
-        } else if primes[i / spf[i]] {
-            let x = r / i;
-            count -= (x - 1) * x;
-        }
+    let mut counts = vec![0; r + 1];
 
-        println!("i: {}, count: {}", i, count);
+    for i in (2..=r).rev() {
+        let x = r / i - (l - 1) / i;
+        counts[i] = x * x;
+        for j in ((2 * i)..=r).step_by(i) {
+            counts[i] -= counts[j];
+        }
+        count += counts[i];
+
+        if 2.max(l) <= i {
+            count -= 2 * x - 1;
+        }
     }
 
     println!("{}", count);
