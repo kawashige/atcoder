7e180c2664a737d93ec2063a59e8b92372b8a51f
Takahiro Kawashige <lapavoni@gmail.com>
2021-06-26 17:17:06 +0900

    Add abc056_d(Subtaskまで)

--- abc056/src/bin/d.rs
+++ abc056/src/bin/d.rs
@@ -1,3 +1,44 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        mut a: [usize; n]
+    }
+
+    a.sort_unstable_by(|a, b| b.cmp(&a));
+
+    let mut set = HashSet::new();
+    let mut dp = vec![HashSet::new(); k];
+
+    let mut count = n;
+
+    for i in 0..n {
+        if a[i] >= k {
+            count -= 1;
+            continue;
+        }
+
+        for j in (0..dp.len()).rev() {
+            if j > 0 && dp[j].is_empty() {
+                continue;
+            }
+            if j + a[i] >= k {
+                dp[j].iter().for_each(|k| {
+                    set.insert(*k);
+                });
+                set.insert(i);
+            } else {
+                for x in dp[j].clone() {
+                    dp[j + a[i]].insert(x);
+                }
+                dp[j + a[i]].insert(i);
+            }
+        }
+    }
+
+    println!("{}", count - set.len());
 }
