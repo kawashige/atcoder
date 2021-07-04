7677a91593d14b3baa2c77ba4fac06fdbc4c8b90
Takahiro Kawashige <lapavoni@gmail.com>
2021-07-04 16:36:04 +0900

    Update typical90_068(解説AC)

--- typical90/src/bin/068.rs
+++ typical90/src/bin/068.rs
@@ -1,56 +1,49 @@
 use proconio::input;
 
-struct SegmentTree {
-    pub node: Vec<i64>,
-    pub n: usize,
+pub struct DisjointSet {
+    parent: Vec<usize>,
+    size: Vec<usize>,
 }
 
-impl SegmentTree {
-    pub fn new(nums: Vec<i64>) -> Self {
-        let size = nums.len();
-
-        let mut n = 1;
-        while n < size {
-            n *= 2;
-        }
-        let mut node = vec![0; 2 * n - 1];
-
-        for i in 0..size {
-            node[i + n - 1] = nums[i];
-        }
-        if n > 2 {
-            for i in (0..=(n - 2)).rev() {
-                node[i] = node[2 * i + 1] + node[2 * i + 2];
-            }
-        }
-
-        Self { node, n }
-    }
-
-    pub fn update(&mut self, i: usize, v: i64) {
-        let mut i = i + self.n - 1;
-
-        self.node[i] = self.node[i] + v;
-        while i > 0 {
-            i = (i - 1) / 2;
-            self.node[i] = self.node[i] + v;
+impl DisjointSet {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            size: vec![1; n],
         }
     }
 
-    pub fn query(&self, a: usize, b: usize, k: usize, l: usize, r: usize) -> i64 {
-        if r <= a || b <= l {
-            return 0;
+    pub fn root(&mut self, i: usize) -> usize {
+        if self.parent[i] == i {
+            return i;
         }
-        if a <= l && r <= b {
-            self.node[k]
-        } else {
-            let c1 = self.query(a, b, 2 * k + 1, l, (l + r) / 2);
-            let c2 = self.query(a, b, 2 * k + 2, (l + r) / 2, r);
-            c1 + c2
+
+        let parent = self.root(self.parent[i]);
+        self.parent[i] = parent;
+        parent
+    }
+
+    pub fn unite(&mut self, i: usize, j: usize) {
+        let mut parent_i = self.root(i);
+        let mut parent_j = self.root(j);
+
+        if parent_i == parent_j {
+            return;
         }
+
+        if self.size(parent_i) < self.size(parent_j) {
+            std::mem::swap(&mut parent_i, &mut parent_j);
+        }
+
+        self.parent[parent_j] = parent_i;
+        self.size[parent_i] += self.size[parent_j];
+    }
+
+    pub fn size(&mut self, i: usize) -> usize {
+        let root = self.root(i);
+        self.size[root]
     }
 }
-
 fn main() {
     input! {
         n: usize,
@@ -58,49 +51,30 @@ fn main() {
         txyv: [(usize, usize, usize, i64); q]
     }
 
-    let mut segtree_even = SegmentTree::new(vec![0; n]);
-    let mut segtree_odd = SegmentTree::new(vec![0; n]);
-    let mut segtree_count = SegmentTree::new(vec![0; n]);
+    let mut sum = vec![0; n];
+    for i in 0..q {
+        if txyv[i].0 == 0 {
+            sum[txyv[i].1 - 1] = txyv[i].3;
+        }
+    }
+
+    let mut potential = vec![0; n];
+    for i in 0..(n - 1) {
+        potential[i + 1] = sum[i] - potential[i];
+    }
+
+    let mut disjoint_set = DisjointSet::new(n);
 
     for (t, x, y, v) in txyv {
         if t == 0 {
-            if x % 2 == 0 {
-                segtree_even.update(x, v);
-            } else {
-                segtree_odd.update(x, v);
-            }
-            segtree_count.update(x, 1);
+            disjoint_set.unite(x - 1, y - 1)
         } else {
-            if x == y {
-                println!("{}", v);
-            } else if x < y {
-                if segtree_count.query(x, y, 0, 0, segtree_count.n) != (y - x) as i64 {
-                    println!("Ambiguous");
-                } else {
-                    let sum = if (y - x) % 2 == 0 { v } else { -v }
-                        + if (y - 1) % 2 == 0 {
-                            segtree_even.query(x, y, 0, 0, segtree_even.n)
-                                - segtree_odd.query(x, y, 0, 0, segtree_even.n)
-                        } else {
-                            -segtree_even.query(x, y, 0, 0, segtree_even.n)
-                                + segtree_odd.query(x, y, 0, 0, segtree_even.n)
-                        };
-                    println!("{}", sum);
-                }
+            if disjoint_set.root(x - 1) != disjoint_set.root(y - 1) {
+                println!("Ambiguous");
+            } else if (x - 1 + y - 1) % 2 == 0 {
+                println!("{}", v + potential[y - 1] - potential[x - 1]);
             } else {
-                if segtree_count.query(y, x, 0, 0, segtree_count.n) != (x - y) as i64 {
-                    println!("Ambiguous");
-                } else {
-                    let sum = if (x - y) % 2 == 0 { v } else { -v }
-                        + if y % 2 == 0 {
-                            segtree_even.query(y, x, 0, 0, segtree_even.n)
-                                - segtree_odd.query(y, x, 0, 0, segtree_even.n)
-                        } else {
-                            -segtree_even.query(y, x, 0, 0, segtree_even.n)
-                                + segtree_odd.query(y, x, 0, 0, segtree_even.n)
-                        };
-                    println!("{}", sum);
-                }
+                println!("{}", potential[y - 1] + potential[x - 1] - v);
             }
         }
     }
