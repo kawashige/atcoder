2ca95afd331c8e35d04401fdf881070f998dc445
Takahiro Kawashige <lapavoni@gmail.com>
2021-07-23 13:57:18 +0900

    Add typical90_015(解説AC)

--- typical90/src/bin/015.rs
+++ typical90/src/bin/015.rs
@@ -1,35 +1,47 @@
 use proconio::input;
 
+fn modinv(a: usize) -> usize {
+    let mut a = a as i64;
+    let m = 1_000_000_007;
+    let mut b = m;
+    let mut u = 1;
+    let mut v = 0;
+    while b > 0 {
+        let t = a / b;
+        a -= t * b;
+        std::mem::swap(&mut a, &mut b);
+        u -= t * v;
+        std::mem::swap(&mut u, &mut v);
+    }
+    u %= m;
+    if u < 0 {
+        u += m;
+    }
+    u as usize
+}
+
 fn main() {
     input! {
         n: usize
     }
-    let m = 1_000_000_007;
+    const M: usize = 1_000_000_007;
 
-    let mut combination = vec![0; n + 1];
-    for i in 2..=n {
-        combination[i] = combination[i - 1] + i - 1;
+    let mut factorial = vec![1; n + 2];
+    for i in 1..factorial.len() {
+        factorial[i] = factorial[i - 1] * i % M;
     }
 
-    println!("combination: {:?}", combination);
-
-    let mut nums = vec![0; n + 1];
-    nums[n] = n;
-
-    for i in (1..n).rev() {
-        nums[i] = nums[i + 1];
-        for j in 1..(1 + i) {
-            if j + i > n {
-                break;
+    for k in 1..=n {
+        let mut r = 0;
+        for i in 1..=(n / k + 1) {
+            if n < (k - 1) * (i - 1) || n - (k - 1) * (i - 1) < i {
+                continue;
             }
-            nums[i] += combination[(n - j) / i + 1];
-            nums[i] %= m;
+            r += (factorial[n - (k - 1) * (i - 1)] * modinv(factorial[i]) % M)
+                * modinv(factorial[n - (k - 1) * (i - 1) - i])
+                % M;
+            r %= M
         }
-    }
-
-    // println!("{:?}", nums);
-
-    for i in 1..=n {
-        println!("{}", nums[i]);
+        println!("{}", r);
     }
 }
