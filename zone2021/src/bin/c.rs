7584210c50ddfec65bf6a8ce21a65a3aaa05f6d8
Takahiro Kawashige <lapavoni@gmail.com>
2021-07-23 15:40:37 +0900

    Add zone2021_c

--- zone2021/src/bin/c.rs
+++ zone2021/src/bin/c.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::HashSet;
 
 use proconio::input;
 
@@ -8,89 +8,58 @@ fn main() {
         v: [[usize; 5]; n]
     }
 
-    let mut a = Vec::with_capacity(n);
-    let mut b = Vec::with_capacity(n);
-    let mut c = Vec::with_capacity(n);
-    let mut d = Vec::with_capacity(n);
-    let mut e = Vec::with_capacity(n);
+    let mut ok = 1;
+    let mut ng = v.iter().map(|vv| vv.iter().max().unwrap()).max().unwrap() + 1;
 
-    for i in 0..n {
-        a.push((v[i][0], i));
-        b.push((v[i][1], i));
-        c.push((v[i][2], i));
-        d.push((v[i][3], i));
-        e.push((v[i][4], i));
-    }
-    a.sort_unstable_by(|a, b| b.0.cmp(&a.0));
-    b.sort_unstable_by(|a, b| b.0.cmp(&a.0));
-    c.sort_unstable_by(|a, b| b.0.cmp(&a.0));
-    d.sort_unstable_by(|a, b| b.0.cmp(&a.0));
-    e.sort_unstable_by(|a, b| b.0.cmp(&a.0));
+    while ok + 1 < ng {
+        let mid = (ok + ng) / 2;
+        let mut set = HashSet::new();
 
-    let mut heap = BinaryHeap::new();
-    heap.push((
-        *vec![a[0].0, b[0].0, c[0].0, d[0].0, e[0].0]
-            .iter()
-            .min()
-            .unwrap(),
-        vec![0, 0, 0, 0, 0],
-    ));
-
-    while let Some((max, v)) = heap.pop() {
-        if vec![a[v[0]].1, b[v[1]].1, c[v[2]].1, d[v[3]].1, e[v[4]].1]
-            .iter()
-            .collect::<HashSet<&usize>>()
-            .len()
-            <= 3
-        {
-            println!("{}", max);
-            return;
+        for i in 0..n {
+            let mut x = 0;
+            for j in 0..5 {
+                if v[i][j] >= mid {
+                    x |= 1 << j;
+                }
+            }
+            set.insert(x);
         }
 
-        if v[0] + 1 < a.len() {
-            heap.push((
-                *vec![a[v[0] + 1].0, b[v[1]].0, c[v[2]].0, d[v[3]].0, e[v[4]].0]
-                    .iter()
-                    .min()
-                    .unwrap(),
-                vec![v[0] + 1, v[1], v[2], v[3], v[4]],
-            ));
+        let mut found = false;
+        let nums = set.into_iter().collect::<Vec<_>>();
+        let target = 2_usize.pow(5) - 1;
+
+        for i in 0..nums.len() {
+            if found {
+                break;
+            }
+            if nums[i] == target {
+                found = true;
+                break;
+            }
+            for j in (i + 1)..nums.len() {
+                if found {
+                    break;
+                }
+                if nums[i] | nums[j] == target {
+                    found = true;
+                    break;
+                }
+                for k in (j + 1)..nums.len() {
+                    if nums[i] | nums[j] | nums[k] == 2_usize.pow(5) - 1 {
+                        found = true;
+                        break;
+                    }
+                }
+            }
         }
-        if v[1] + 1 < b.len() {
-            heap.push((
-                *vec![a[v[0]].0, b[v[1] + 1].0, c[v[2]].0, d[v[3]].0, e[v[4]].0]
-                    .iter()
-                    .min()
-                    .unwrap(),
-                vec![v[0], v[1] + 1, v[2], v[3], v[4]],
-            ));
-        }
-        if v[2] + 1 < c.len() {
-            heap.push((
-                *vec![a[v[0]].0, b[v[1]].0, c[v[2] + 1].0, d[v[3]].0, e[v[4]].0]
-                    .iter()
-                    .min()
-                    .unwrap(),
-                vec![v[0], v[1], v[2] + 1, v[3], v[4]],
-            ));
-        }
-        if v[3] + 1 < d.len() {
-            heap.push((
-                *vec![a[v[0]].0, b[v[1]].0, c[v[2]].0, d[v[3] + 1].0, e[v[4]].0]
-                    .iter()
-                    .min()
-                    .unwrap(),
-                vec![v[0], v[1], v[2], v[3] + 1, v[4]],
-            ));
-        }
-        if v[4] + 1 < e.len() {
-            heap.push((
-                *vec![a[v[0]].0, b[v[1]].0, c[v[2]].0, d[v[3]].0, e[v[4] + 1].0]
-                    .iter()
-                    .min()
-                    .unwrap(),
-                vec![v[0], v[1], v[2], v[3], v[4] + 1],
-            ));
+
+        if found {
+            ok = mid;
+        } else {
+            ng = mid;
         }
     }
+
+    println!("{}", ok);
 }
