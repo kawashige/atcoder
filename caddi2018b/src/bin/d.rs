8d708df7617121fce235344abad05f331ffc569f
Takahiro Kawashige <lapavoni@gmail.com>
2021-07-24 19:19:58 +0900

    Add caddi2018_d(解説AC)

--- caddi2018b/src/bin/d.rs
+++ caddi2018b/src/bin/d.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [usize; n]
+       a: [usize; n]
+    }
+
+    if a.iter().all(|x| x % 2 == 0) {
+        println!("second");
+    } else {
+        println!("first");
     }
 }
