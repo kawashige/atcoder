2c183484e3038c5d82c036b92cb874b5f30e1954
Takahiro Kawashige <lapavoni@gmail.com>
2021-07-25 13:13:33 +0900

    Add typical90_011

--- typical90/src/bin/011.rs
+++ typical90/src/bin/011.rs
@@ -1,3 +1,28 @@
+use std::usize;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut dcs: [(usize, usize, u64); n]
+    }
+
+    dcs.sort_unstable();
+
+    let mut dp = vec![0; 5001];
+
+    for i in 0..n {
+        if dcs[i].0 < dcs[i].1 {
+            continue;
+        }
+        for j in (1..(dcs[i].0 - dcs[i].1 + 1).min(dp.len())).rev() {
+            if dp[j] > 0 {
+                dp[j + dcs[i].1] = dp[j + dcs[i].1].max(dp[j] + dcs[i].2);
+            }
+        }
+        dp[dcs[i].1] = dp[dcs[i].1].max(dcs[i].2);
+    }
+
+    println!("{}", dp.iter().max().unwrap());
 }
