79aad4470f9a81ac4cbfbdf5e334960faf003877
Takahiro Kawashige <lapavoni@gmail.com>
2021-07-31 14:40:24 +0900

    Add abc210_e(解説AC)

--- abc210/src/bin/e.rs
+++ abc210/src/bin/e.rs
@@ -1,5 +1,12 @@
 use proconio::input;
 
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
 fn main() {
     input! {
         n: usize,
@@ -7,5 +14,21 @@ fn main() {
         mut ac: [(usize, usize); m]
     }
 
-    let mut dp1 = vec![vec![0; w]; h];
+    ac.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+
+    let mut r = 0;
+    let mut x = n;
+    for i in 0..m {
+        let tmp = gcd(x, ac[i].0);
+        r += (x - tmp) * ac[i].1;
+
+        if i == m - 1 && tmp > 1 {
+            println!("-1");
+            return;
+        }
+
+        x = tmp;
+    }
+
+    println!("{}", r)
 }
