47512560903dd253fec15e08c25cffedf75c4cf4
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-05 21:17:34 +0900

    Update abc034_d

--- abc034/src/bin/d.rs
+++ abc034/src/bin/d.rs
@@ -7,31 +7,27 @@ fn main() {
         mut wp: [(f64, f64); n]
     }
 
-    wp.sort_unstable_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+    let mut ok = 0.0;
+    let mut ng = 100.0;
 
-    let mut used = vec![false; n];
-    used[0] = true;
-    let mut w = wp[0].0;
-    let mut p = wp[0].1;
+    for _ in 0..100 {
+        let mid = (ok + ng) / 2.0;
 
-    for _ in 0..(k - 1) {
-        let mut max_i = 0;
-        let mut max_p = 0.0;
-        for i in 0..n {
-            if used[i] {
-                continue;
-            }
-            let tmp_p = (wp[i].0 * wp[i].1 + w * p) / (wp[i].0 + w);
-            if max_p < tmp_p {
-                max_p = tmp_p;
-                max_i = i;
-            }
+        wp.sort_unstable_by(|a, b| {
+            (b.0 * (b.1 - mid))
+                .partial_cmp(&(a.0 * (a.1 - mid)))
+                .unwrap()
+        });
+
+        let (_, p) = wp.iter().take(k).fold((0.0, 0.0), |(w, p), (w1, p1)| {
+            (w + w1, (w * p + w1 * p1) / (w + w1))
+        });
+        if p < mid {
+            ng = mid
+        } else {
+            ok = mid
         }
-
-        used[max_i] = true;
-        w += wp[max_i].0;
-        p = max_p;
     }
 
-    println!("{}", p);
+    println!("{}", ok);
 }
