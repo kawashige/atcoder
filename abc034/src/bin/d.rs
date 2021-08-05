fb7633eaa2d506dc100f358d919dcf82bcb3ea3b
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-05 20:55:11 +0900

    Add abc034_d

--- abc034/src/bin/d.rs
+++ abc034/src/bin/d.rs
@@ -4,27 +4,34 @@ fn main() {
     input! {
         n: usize,
         k: usize,
-        mut wp: [(f64, usize); n]
+        mut wp: [(f64, f64); n]
     }
 
-    wp.sort_unstable_by(|a, b| b.1.cmp(&a.1));
+    wp.sort_unstable_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
 
-    let mut dp = vec![(0.0, 0.0); k + 1];
+    let mut used = vec![false; n];
+    used[0] = true;
+    let mut w = wp[0].0;
+    let mut p = wp[0].1;
 
-    for i in 0..n {
-        let (w, p) = &wp[i];
-        for j in (0..k).rev() {
-            if dp[j].0 > 0.0 {
-                let new_p = (dp[j].0 * dp[j].1 + w * (*p as f64)) / (dp[j].1 + w);
-                if dp[j + 1].0 < new_p {
-                    dp[j + 1] = (new_p, w + dp[j].1);
-                }
+    for _ in 0..(k - 1) {
+        let mut max_i = 0;
+        let mut max_p = 0.0;
+        for i in 0..n {
+            if used[i] {
+                continue;
+            }
+            let tmp_p = (wp[i].0 * wp[i].1 + w * p) / (wp[i].0 + w);
+            if max_p < tmp_p {
+                max_p = tmp_p;
+                max_i = i;
             }
         }
-        if dp[1].0 < *p as f64 {
-            dp[1] = (*p as f64, *w);
-        }
+
+        used[max_i] = true;
+        w += wp[max_i].0;
+        p = max_p;
     }
 
-    println!("{}", dp[k].0);
+    println!("{}", p);
 }
