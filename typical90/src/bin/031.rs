a1eb27e001af03c6cfb297b2c91d8ba0368a2d15
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-09 17:46:13 +0900

    Add typical90_031(解説AC)

--- typical90/src/bin/031.rs
+++ typical90/src/bin/031.rs
@@ -1,27 +1,4 @@
-use std::collections::HashMap;
-
 use proconio::input;
-
-fn recurse(w: usize, b: usize, memo: &mut HashMap<(usize, usize), bool>) -> bool {
-    if w == 0 && b < 2 {
-        return false;
-    }
-
-    if let Some(b) = memo.get(&(w, b)) {
-        return *b;
-    }
-
-    let mut result = true;
-    if w > 0 {
-        result &= recurse(w - 1, b + w, memo);
-    }
-    for i in 1..=(b / 2) {
-        result &= recurse(w, b - i, memo);
-    }
-
-    memo.insert((w, b), !result);
-    !result
-}
 
 fn main() {
     input! {
@@ -30,15 +7,32 @@ fn main() {
         b: [usize; n],
     }
 
-    let mut memo = HashMap::new();
-    let mut count = 0;
-    for i in 0..n {
-        if recurse(w[i], b[i], &mut memo) {
-            count += 1;
-        } else {
-            count += 2;
+    let mut grundy = vec![vec![0; 1555]; 55];
+
+    for i in 0..51 {
+        for j in 0..1501 {
+            let mut mex = vec![0; 1555];
+            if i >= 1 {
+                mex[grundy[i - 1][j + i]] = 1;
+            }
+            if j >= 2 {
+                for k in 1..=(j / 2) {
+                    mex[grundy[i][j - k]] = 1;
+                }
+            }
+            for k in 0..=1555 {
+                if mex[k] == 0 {
+                    grundy[i][j] = k;
+                    break;
+                }
+            }
         }
     }
 
-    println!("{}", if count % 2 == 0 { "Second" } else { "First" });
+    let mut sum_xor = 0;
+    for i in 0..n {
+        sum_xor ^= grundy[w[i]][b[i]];
+    }
+
+    println!("{}", if sum_xor == 0 { "Second" } else { "First" });
 }
