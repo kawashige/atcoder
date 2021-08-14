ffce345c350b4e5002d2961aece4b540b25101d4
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-14 14:20:13 +0900

    Add abc130_e(解説AC)

--- abc130/src/bin/e.rs
+++ abc130/src/bin/e.rs
@@ -1,3 +1,41 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        s: [i64; n],
+        t: [i64; m]
+    }
+
+    const M: i64 = 1_000_000_007;
+
+    let mut dp = vec![vec![0; m + 1]; n + 1];
+    dp[0][0] = 1;
+
+    for i in 0..=n {
+        for j in 0..=m {
+            if i > 0 && j > 0 && s[i - 1] == t[j - 1] {
+                dp[i][j] += dp[i - 1][j - 1];
+                dp[i][j] %= M;
+            }
+            if i > 0 {
+                dp[i][j] += dp[i - 1][j];
+                dp[i][j] %= M;
+            }
+            if j > 0 {
+                dp[i][j] += dp[i][j - 1];
+                dp[i][j] %= M;
+            }
+            if i > 0 && j > 0 {
+                dp[i][j] -= dp[i - 1][j - 1];
+                dp[i][j] %= M;
+                if dp[i][j] < 0 {
+                    dp[i][j] += M;
+                }
+            }
+        }
+    }
+
+    println!("{}", dp[n][m]);
 }
