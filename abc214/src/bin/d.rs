841f6438e92866e8fb0143b7167aa6636824186a
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-15 19:19:47 +0900

    Add abc214_d(解説AC)

--- abc214/src/bin/d.rs
+++ abc214/src/bin/d.rs
@@ -1,15 +1,47 @@
 use proconio::input;
+pub struct DisjointSet {
+    parent: Vec<usize>,
+    size: Vec<usize>,
+}
 
-fn dfs(i: usize, list: &Vec<Vec<(usize, u64)>>, seen: &mut Vec<bool>, w: u64) -> usize {
-    seen[i] = true;
-    let mut count = 1;
-    for (child, weight) in &list[i] {
-        if seen[*child] || weight > &w {
-            continue;
+impl DisjointSet {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            size: vec![1; n],
         }
-        count += dfs(*child, list, seen, w);
     }
-    count
+
+    pub fn root(&mut self, i: usize) -> usize {
+        if self.parent[i] == i {
+            return i;
+        }
+
+        let parent = self.root(self.parent[i]);
+        self.parent[i] = parent;
+        parent
+    }
+
+    pub fn unite(&mut self, i: usize, j: usize) {
+        let mut parent_i = self.root(i);
+        let mut parent_j = self.root(j);
+
+        if parent_i == parent_j {
+            return;
+        }
+
+        if self.size(parent_i) < self.size(parent_j) {
+            std::mem::swap(&mut parent_i, &mut parent_j);
+        }
+
+        self.parent[parent_j] = parent_i;
+        self.size[parent_i] += self.size[parent_j];
+    }
+
+    pub fn size(&mut self, i: usize) -> usize {
+        let root = self.root(i);
+        self.size[root]
+    }
 }
 
 fn main() {
@@ -18,23 +50,14 @@ fn main() {
         mut uvw: [(usize, usize, u64); n - 1]
     }
 
-    uvw.sort_unstable_by(|a, b| b.2.cmp(&a.2));
+    uvw.sort_unstable_by(|a, b| a.2.cmp(&b.2));
 
-    let mut list = vec![vec![]; n];
-    for (u, v, w) in &uvw {
-        list[u - 1].push((v - 1, *w));
-        list[v - 1].push((u - 1, *w));
-    }
-
+    let mut disjoint_set = DisjointSet::new(n);
     let mut r = 0;
-    for (a, b, w) in uvw {
-        let mut seen = vec![false; n];
-        if seen[a - 1] || seen[b - 1] {
-            continue;
-        }
-        let c = dfs(a - 1, &list, &mut seen, w);
 
-        r += (c - 1) as u64 * w;
+    for (u, v, w) in uvw {
+        r += disjoint_set.size(u - 1) as u64 * disjoint_set.size(v - 1) as u64 * w;
+        disjoint_set.unite(u - 1, v - 1);
     }
 
     println!("{}", r);
