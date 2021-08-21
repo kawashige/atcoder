9d8531e074d7276503a538f5d4eb6ec336eb9619
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-21 23:54:30 +0900

    Add abc215_e(解説AC)

--- abc215/src/bin/e.rs
+++ abc215/src/bin/e.rs
@@ -3,69 +3,37 @@ use proconio::marker::Chars;
 
 const M: usize = 998244353;
 
-fn mod_pow(x: usize, n: usize) -> usize {
-    if n == 0 {
-        1
-    } else if n % 2 == 0 {
-        let r = mod_pow(x, n / 2);
-        r * r % M
-    } else {
-        x * mod_pow(x, n - 1) % M
-    }
-}
-
 fn main() {
     input! {
         n: usize,
         s: Chars
     }
 
-    // let mut count = vec![0; 10];
-    // for c in &s {
-    //     count[*c as usize - b'A' as usize] += 1;
-    // }
-
     let mut r = 0;
-
-    // for c in &count {
-    //     r += mod_pow(2, *c) - 1;
-    //     r %= M;
-    // }
-
-    // println!("r1: {}", r);
-
-    let mut dp = vec![vec![vec![0; 1 << 10]; 10]; n + 1];
+    let mut dp = vec![vec![vec![0; 10]; 1 << 10]; n + 1];
 
     for i in 0..n {
-        for j in 0..10 {
-            for k in 1..dp[0].len() {
-                if j == (s[i] as usize - b'A' as usize) {
-                    if dp[i][j][k] > 0 {
-                        dp[i + 1][j][k] += (dp[i][j][k] * 2 + 1) % M;
-                        dp[i + 1][j][k] %= M;
-                    }
-                } else {
-                    if dp[i][j][k] > 0 {
-                        dp[i + 1][j][k] += dp[i][j][k];
-                        dp[i + 1][j][k] %= M;
-                        let x = 1 << (s[i] as usize - b'A' as usize);
-                        if k & x == 0 {
-                            dp[i + 1][s[i] as usize - b'A' as usize][k | x] += dp[i][j][k];
-                            dp[i + 1][s[i] as usize - b'A' as usize][k | x] %= M;
-                        }
+        let idx = s[i] as usize - b'A' as usize;
+        for j in 1..dp[0].len() {
+            for k in 0..10 {
+                if dp[i][j][k] > 0 {
+                    dp[i + 1][j][k] += dp[i][j][k];
+                    dp[i + 1][j][k] %= M;
+
+                    if idx == k || j & 1 << idx == 0 {
+                        dp[i + 1][j | 1 << idx][idx] += dp[i][j][k];
+                        dp[i + 1][j | 1 << idx][idx] %= M;
                     }
                 }
             }
         }
-        dp[i + 1][s[i] as usize - b'A' as usize][1 << (s[i] as usize - b'A' as usize)] += 1;
-        dp[i + 1][s[i] as usize - b'A' as usize][1 << (s[i] as usize - b'A' as usize)] %= M;
+        dp[i + 1][1 << idx][idx] += 1;
+        dp[i + 1][1 << idx][idx] %= M;
     }
 
-    // println!("dp: {:?}", dp);
-
-    for i in 0..dp[0][0].len() {
+    for i in 0..(1 << 10) {
         for j in 0..10 {
-            r += dp[n][j][i];
+            r += dp[n][i][j];
             r %= M;
         }
     }
