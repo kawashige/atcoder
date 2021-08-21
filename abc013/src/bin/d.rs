e79147cbdc2273b8eabe7cad6094cd30f1bf4bcd
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-21 13:00:16 +0900

    Add abc013_d

--- abc013/src/bin/d.rs
+++ abc013/src/bin/d.rs
@@ -1,3 +1,45 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        d: usize,
+        a: [usize; m]
+    }
+
+    let mut dest = (0..n).collect::<Vec<_>>();
+    for x in a {
+        dest.swap(x - 1, x);
+    }
+
+    let mut dest2 = vec![0; n];
+    for i in 0..n {
+        dest2[dest[i]] = i;
+    }
+
+    let mut r = vec![0; n];
+    let mut seen = vec![false; n];
+
+    for i in 0..n {
+        if seen[i] {
+            continue;
+        }
+
+        let mut v = Vec::new();
+        let mut idx = i;
+        while !seen[idx] {
+            seen[idx] = true;
+            v.push(idx);
+            idx = dest2[idx];
+        }
+
+        for i in 0..v.len() {
+            r[v[i]] = v[(i + d % v.len()) % v.len()];
+        }
+    }
+
+    for x in r {
+        println!("{}", x + 1);
+    }
 }
