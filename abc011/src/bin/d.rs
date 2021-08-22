dc36338ba51216d8c80ce94e64ddbf7cc8d4d462
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-22 15:22:39 +0900

    Update abc011_d(解説AC)

--- abc011/src/bin/d.rs
+++ abc011/src/bin/d.rs
@@ -2,7 +2,7 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
         d: i32,
         x: i32,
         y: i32
@@ -16,33 +16,30 @@ fn main() {
     let c_x = x.abs() / d;
     let c_y = y.abs() / d;
 
-    if (n - c_x - c_y) % 2 != 0 {
+    if (n as i32 - c_x - c_y) % 2 != 0 {
         println!("0");
         return;
     }
 
-    let mut factorial = vec![1; n as usize + 1];
-    for i in 2..=(n as usize) {
-        factorial[i] = factorial[i - 1] * i as u128;
+    let mut combination = vec![vec![0.0; n + 1]; n + 1];
+    for i in 0..(n + 1) {
+        combination[i][0] = 1.0 / 2.0_f64.powf(i as f64);
+    }
+    for i in 1..(n + 1) {
+        for j in 1..i + 1 {
+            combination[i][j] = combination[i - 1][j] + combination[i - 1][j - 1];
+            combination[i][j] /= 2.0;
+        }
     }
 
-    let mut c = 0;
-    let r = (n - c_x - c_y) / 2;
+    let mut c = 0.0;
+    let r = (n as i32 - c_x - c_y) / 2;
 
     for i in 0..=r {
         let j = r - i;
-        let c1 = factorial[n as usize]
-            / (factorial[(n - (c_x + i)) as usize] * factorial[(c_x + i) as usize]);
-        let c2 = factorial[(n - (c_x + i)) as usize]
-            / (factorial[(n - (c_x + i) - i) as usize] * factorial[i as usize]);
-        let c3 = factorial[(n - (c_x + i) - i) as usize]
-            / (factorial[(n - (c_x + i) - i - (c_y + j)) as usize] * factorial[(c_y + j) as usize]);
-        c += c1 * c2 * c3;
-    }
-
-    let mut c = c as f64;
-    for _ in 0..n {
-        c /= 4.0;
+        c += combination[n][(c_x + i * 2) as usize]
+            * combination[(c_x + i * 2) as usize][i as usize]
+            * combination[n - (c_x + i * 2) as usize][j as usize];
     }
 
     println!("{}", c);
