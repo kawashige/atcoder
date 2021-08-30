6426e21087373b825ed1e0270b1896c796396a80
Takahiro Kawashige <lapavoni@gmail.com>
2021-08-30 21:13:43 +0900

    Add abc004_d(解説AC)

--- abc004/src/bin/d.rs
+++ abc004/src/bin/d.rs
@@ -7,37 +7,30 @@ fn main() {
         b: usize
     }
 
-    let mut min = std::usize::MAX;
-    let mut acc = vec![0; 601];
-    for i in 1..acc.len() {
-        acc[i] = i + acc[i - 1];
+    let skip = vec![0, b, b + g, b + g + r];
+    let mut dp = vec![vec![std::i32::MAX; r + g + b + 1]; 2001];
+    dp[0][r + g + b] = 0;
+
+    for i in 0..(dp.len() - 1) {
+        for j in 0..dp[0].len() {
+            if dp[i][j] != std::i32::MAX {
+                if skip.contains(&j) {
+                    dp[i + 1][j] = dp[i + 1][j].min(dp[i][j]);
+                }
+                if j > 0 {
+                    let origin = if j <= b {
+                        100
+                    } else if j <= b + g {
+                        0
+                    } else {
+                        -100
+                    };
+                    let m = (i as i32 - 1000 - origin).abs();
+                    dp[i + 1][j - 1] = dp[i + 1][j - 1].min(dp[i][j] + m);
+                }
+            }
+        }
     }
 
-    for i in 0..(g - 1) {
-        let mut c = 0;
-
-        // g
-        c += acc[i] + acc[g - 1 - i];
-
-        // r
-        c += if i <= 99 {
-            let right = (99 - i).min((r - 1) / 2);
-            acc[right] + acc[r - 1 - right]
-        } else {
-            acc[r + (i - 100)] - acc[i - 100]
-        };
-
-        // b
-        let j = g - 1 - i;
-        c += if j <= 99 {
-            let right = (99 - j).min((b - 1) / 2);
-            acc[right] + acc[b - 1 - right]
-        } else {
-            acc[b + (j - 100)] - acc[j - 100]
-        };
-
-        min = min.min(c);
-    }
-
-    println!("{}", min);
+    println!("{}", dp[2000][0]);
 }
