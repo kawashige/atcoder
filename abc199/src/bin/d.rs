f1eb00bd0a48b5749f2e7b379b608c143d94b10e
Takahiro Kawashige <lapavoni@gmail.com>
2021-09-09 21:17:50 +0900

    Add abc199_d(解説AC)

--- abc199/src/bin/d.rs
+++ abc199/src/bin/d.rs
@@ -1,6 +1,35 @@
-use alloc::collections;
 use proconio::input;
-use std::collections::HashSet;
+
+fn dfs(i: usize, list: &Vec<Vec<usize>>, seen: &mut Vec<bool>, nodes: &mut Vec<usize>) {
+    nodes.push(i);
+    seen[i] = true;
+
+    for next in &list[i] {
+        if seen[*next] {
+            continue;
+        }
+        dfs(*next, list, seen, nodes);
+    }
+}
+
+fn dfs2(nodes: &Vec<usize>, colors: &mut Vec<usize>, matrix: &Vec<Vec<bool>>, r: &mut u64) {
+    if nodes.len() == colors.len() {
+        *r += 1;
+        return;
+    }
+
+    for j in 0..3 {
+        if (0..colors.len()).any(|k| matrix[nodes[colors.len()]][nodes[k]] && colors[k] == j) {
+            continue;
+        }
+
+        colors.push(j);
+
+        dfs2(nodes, colors, matrix, r);
+
+        colors.pop();
+    }
+}
 
 fn main() {
     input! {
@@ -9,19 +38,29 @@ fn main() {
         ab: [(usize, usize); m]
     }
 
+    let mut matrix = vec![vec![false; n]; n];
     let mut lists = vec![vec![]; n];
     for (a, b) in ab {
+        matrix[a - 1][b - 1] = true;
+        matrix[b - 1][a - 1] = true;
+
         lists[a - 1].push(b - 1);
         lists[b - 1].push(a - 1);
     }
 
-    if lists.is_empty() {
-        println!("{}", 3_u64.pow(n as u32));
-        return;
+    let mut seen = vec![false; n];
+    let mut r: u64 = 1;
+
+    for i in 0..n {
+        if seen[i] {
+            continue;
+        }
+        let mut nodes = Vec::new();
+        dfs(i, &lists, &mut seen, &mut nodes);
+        let mut x = 0;
+        dfs2(&nodes, &mut Vec::new(), &matrix, &mut x);
+        r *= x;
     }
 
-    if lists.iter().any(|v| v.len() >= 4) {
-        println!("0");
-        return;
-    }
+    println!("{}", r);
 }
