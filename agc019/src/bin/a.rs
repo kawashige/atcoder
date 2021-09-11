268c7e301681b032a22d7965b9cf3307c9c10cb0
Takahiro Kawashige <lapavoni@gmail.com>
2021-09-11 10:31:58 +0900

    Add agc019_a

--- agc019/src/bin/a.rs
+++ agc019/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        q: u64,
+        h: u64,
+        s: u64,
+        d: u64,
+        n: u64,
+    }
+
+    let one = (q * 4).min(h * 2).min(s);
+    let two = (one * 2).min(d);
+
+    println!("{}", (n / 2) * two + (n % 2) * one);
 }
