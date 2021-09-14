db632bb07254a3fb048174472fdbbe9f826ec699
Takahiro Kawashige <lapavoni@gmail.com>
2021-09-14 20:26:39 +0900

    Add arc110_a

--- arc110/src/bin/a.rs
+++ arc110/src/bin/a.rs
@@ -1,9 +1,20 @@
 use proconio::input;
 
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a / gcd(a, b) * b
+}
 fn main() {
     input! {
         n: u64
     }
 
-    println!("{}", (2..=n).product::<u64>() + 1);
+    println!("{}", (2..=n).fold(1, |acc, x| lcm(acc, x)) + 1);
 }
