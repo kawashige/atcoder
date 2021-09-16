428895b58541893000d45193bf6a6042f8f2e6ce
Takahiro Kawashige <lapavoni@gmail.com>
2021-09-16 20:31:19 +0900

    Add abc151_f(解説AC)

--- abc151/src/bin/f.rs
+++ abc151/src/bin/f.rs
@@ -1,37 +1,55 @@
 use proconio::input;
 
-fn main() {
-    input! {
-        n: usize,
-        xy: [(i64, i64); n]
-    }
+fn intersection(r: f64, a: (f64, f64), b: (f64, f64)) -> Vec<(f64, f64)> {
+    let c = ((a.0 + b.0) / 2.0, (a.1 + b.1) / 2.0);
+    let dist = calc_dist(a, b);
+    let h = (r * r - (dist / 2.0) * (dist / 2.0)).sqrt();
+    let aa = ((a.0 - b.0) / dist, (a.1 - b.1) / dist);
+    let hp = (-aa.1 * h, aa.0 * h);
+    let hm = (aa.1 * h, -aa.0 * h);
 
-    let mut max = 0;
-    let mut p = (0, 0);
+    vec![(c.0 + hp.0, c.1 + hp.1), (c.0 + hm.0, c.1 + hm.1)]
+}
+
+fn calc_dist(a: (f64, f64), b: (f64, f64)) -> f64 {
+    ((a.0 - b.0).powf(2.0) + (a.1 - b.1).powf(2.0)).sqrt()
+}
+
+fn check(r: f64, xy: &Vec<(f64, f64)>) -> bool {
+    let n = xy.len();
     for i in 0..n {
-        for j in 0..n {
-            let d = (xy[i].0 - xy[j].0) * (xy[i].0 - xy[j].0)
-                + (xy[i].1 - xy[j].1) * (xy[i].1 - xy[j].1);
-            if d > max {
-                max = d;
-                p = (i, j);
+        for j in (i + 1)..n {
+            if calc_dist(xy[i], xy[j]) / 2.0 > r {
+                continue;
+            }
+
+            for p in intersection(r, xy[i], xy[j]) {
+                if (0..n).all(|k| k == i || k == j || calc_dist(xy[k], p) <= r) {
+                    return true;
+                }
             }
         }
     }
+    false
+}
 
-    let (gx, gy) = (
-        (xy[p.0].0 + xy[p.1].0) as f64 / 2.0,
-        (xy[p.0].1 + xy[p.1].1) as f64 / 2.0,
-    );
-    let mut max = std::f64::MIN;
-    for i in 0..n {
-        let d = ((gx - xy[i].0 as f64) * (gx - xy[i].0 as f64)
-            + (gy - xy[i].1 as f64) * (gy - xy[i].1 as f64))
-            .sqrt();
-        if d > max {
-            max = d;
+fn main() {
+    input! {
+        n: usize,
+        xy: [(f64, f64); n]
+    }
+
+    let mut ok = 1000.0;
+    let mut ng = 0.0;
+
+    while ok - ng > 0.0000001 {
+        let mid = (ok + ng) / 2.0;
+        if check(mid, &xy) {
+            ok = mid;
+        } else {
+            ng = mid
         }
     }
 
-    println!("{}", max);
+    println!("{}", ok);
 }
