d3c97464e3c51c88d5985719cfd1f768c7763b3d
Takahiro Kawashige <lapavoni@gmail.com>
2021-09-23 21:39:28 +0900

    Add abc171_f(TLE)

--- abc171/src/bin/f.rs
+++ abc171/src/bin/f.rs
@@ -1,3 +1,43 @@
+use std::collections::HashMap;
+
+use proconio::input;
+use proconio::marker::Chars;
+
+const M: usize = 1_000_000_007;
+
+fn dfs(i: usize, j: usize, l: usize, memo: &mut HashMap<(usize, usize), usize>) -> usize {
+    if i == 0 {
+        if j == 0 {
+            1
+        } else {
+            0
+        }
+    } else if i < j {
+        0
+    } else {
+        if let Some(x) = memo.get(&(i, j)) {
+            *x
+        } else {
+            let mut r = if j == 0 {
+                0
+            } else {
+                dfs(i - 1, j - 1, l, memo)
+            };
+            r += dfs(i - 1, j, l, memo) * if j == l { 26 } else { 25 } % M;
+            r %= M;
+            memo.insert((i, j), r);
+            r
+        }
+    }
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        k: usize,
+        s: Chars
+    }
+
+    let r = dfs(k + s.len(), s.len(), s.len(), &mut HashMap::new());
+
+    println!("{}", r);
 }
