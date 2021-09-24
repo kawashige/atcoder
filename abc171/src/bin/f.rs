73b2fca1947ec881df463b25c67697f3d098525d
Takahiro Kawashige <lapavoni@gmail.com>
2021-09-24 11:01:24 +0900

    Add abc171_f(解説AC)

--- abc171/src/bin/f.rs
+++ abc171/src/bin/f.rs
@@ -1,34 +1,26 @@
-use std::collections::HashMap;
-
 use proconio::input;
 use proconio::marker::Chars;
 
 const M: usize = 1_000_000_007;
 
-fn dfs(i: usize, j: usize, l: usize, memo: &mut HashMap<(usize, usize), usize>) -> usize {
-    if i == 0 {
-        if j == 0 {
-            1
-        } else {
-            0
-        }
-    } else if i < j {
-        0
-    } else {
-        if let Some(x) = memo.get(&(i, j)) {
-            *x
-        } else {
-            let mut r = if j == 0 {
-                0
-            } else {
-                dfs(i - 1, j - 1, l, memo)
-            };
-            r += dfs(i - 1, j, l, memo) * if j == l { 26 } else { 25 } % M;
-            r %= M;
-            memo.insert((i, j), r);
-            r
-        }
+fn modinv(a: usize) -> usize {
+    let mut a = a as i64;
+    let m = 1_000_000_007;
+    let mut b = m;
+    let mut u = 1;
+    let mut v = 0;
+    while b > 0 {
+        let t = a / b;
+        a -= t * b;
+        std::mem::swap(&mut a, &mut b);
+        u -= t * v;
+        std::mem::swap(&mut u, &mut v);
     }
+    u %= m;
+    if u < 0 {
+        u += m;
+    }
+    u as usize
 }
 
 fn main() {
@@ -37,7 +29,29 @@ fn main() {
         s: Chars
     }
 
-    let r = dfs(k + s.len(), s.len(), s.len(), &mut HashMap::new());
+    let n = s.len();
+    let mut factorial = vec![1; k + n + 1];
+    let mut mod_factorial = vec![1; k + n + 1];
+    for i in 2..factorial.len() {
+        factorial[i] = (factorial[i - 1] * i) % M;
+        mod_factorial[i] = modinv(factorial[i]);
+    }
+
+    let mut pow25 = vec![1; k + 1];
+    let mut pow26 = vec![1; k + 1];
+    for i in 1..=k {
+        pow25[i] = pow25[i - 1] * 25 % M;
+        pow26[i] = pow26[i - 1] * 26 % M;
+    }
+
+    let mut r = 0;
+
+    for i in 0..=k {
+        let mut c = (factorial[n + i - 1] * mod_factorial[n - 1] % M) * mod_factorial[i] % M;
+        c *= pow25[i] * pow26[k - i] % M;
+        r += c % M;
+        r %= M
+    }
 
     println!("{}", r);
 }
