0150b7309fe061989a760492202f646c342b2f50
Takahiro Kawashige <lapavoni@gmail.com>
2021-09-30 21:44:15 +0900

    Add abc220_e(解説AC)

--- abc220/src/bin/e.rs
+++ abc220/src/bin/e.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
+fn f(b: i64, x: i64) -> i64 {
+    0.max(b - (x - (b + 1)).abs())
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: i64,
+        d: i64
+    }
+
+    const M: i64 = 998244353;
+
+    let mut pow = vec![1; 2_000_001];
+    for i in 1..pow.len() {
+        pow[i] = pow[i - 1] * 2 % M;
+    }
+
+    let mut r = 0;
+
+    if d != 1 {
+        for i in 0..n {
+            r += (pow[i as usize] * pow[d as usize - 2] % M) * f(n - 1 - i, d) % M;
+            r %= M;
+        }
+    }
+
+    if n > d {
+        for i in 0..(n - d) {
+            r += pow[i as usize] * pow[d as usize] % M;
+            r %= M;
+        }
+    }
+
+    println!("{}", r * 2 % M);
 }
