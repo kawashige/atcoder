27f8cb1951df568039a36b6250a6276705219b6d
Takahiro Kawashige <lapavoni@gmail.com>
2021-11-19 20:59:53 +0900

    Add arc040_c

--- arc040/src/bin/c.rs
+++ arc040/src/bin/c.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut s: [Chars; n]
+    }
+
+    let mut count = 0;
+    for i in 0..n {
+        if let Some(j) = (0..s[i].len()).rev().find(|j| s[i][*j] == '.') {
+            count += 1;
+            if i < n - 1 {
+                for k in j..s[i + 1].len() {
+                    s[i + 1][k] = 'o';
+                }
+            }
+        }
+    }
+
+    println!("{}", count);
 }
