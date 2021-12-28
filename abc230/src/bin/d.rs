4605bd0f0dbc14051c08c19082545c81b95521b5
Takahiro Kawashige <lapavoni@gmail.com>
2021-12-28 15:57:05 +0900

    Add abc230_d_e

--- abc230/src/bin/d.rs
+++ abc230/src/bin/d.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        d: usize,
+        mut lr: [(usize, usize); n]
+    }
+
+    lr.sort_unstable_by_key(|(_, r)| *r);
+
+    let mut i = 0;
+    let mut broken = 0;
+    let mut count = 0;
+
+    while i < n {
+        if broken < lr[i].0 {
+            count += 1;
+            broken = lr[i].1 + d - 1;
+        }
+        i += 1;
+    }
+
+    println!("{}", count);
 }
--- abc230/src/bin/e.rs
+++ abc230/src/bin/e.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64
+    }
+
+    let mut sum = 0;
+    let mut i = 1;
+
+    while i <= n {
+        sum += (n / i - n / (i + 1)) * i;
+        if n / (i + 1) == 0 {
+            break;
+        }
+        i = n / (n / (i + 1));
+    }
+
+    println!("{}", sum);
 }
