ed5ab625c3564ede01d2447d73fc75adb975d9b6
Takahiro Kawashige <lapavoni@gmail.com>
2022-01-02 14:30:44 +0900

    Add abc233_e

--- abc233/src/bin/e.rs
+++ abc233/src/bin/e.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: String
+    }
+
+    let mut sum = x
+        .chars()
+        .map(|d| d.to_digit(10).unwrap() as usize)
+        .sum::<usize>();
+
+    let mut result = Vec::with_capacity(x.len());
+    result.push((b'0' + (sum % 10) as u8) as char);
+    let mut carry = sum / 10;
+
+    for c in x.chars().rev() {
+        sum -= c.to_digit(10).unwrap() as usize;
+        result.push((b'0' + ((sum + carry) % 10) as u8) as char);
+        carry = (sum + carry) / 10;
+    }
+
+    for c in carry.to_string().chars().rev() {
+        result.push(c);
+    }
+
+    while result.last() == Some(&'0') {
+        result.pop();
+    }
+
+    println!("{}", result.into_iter().rev().collect::<String>());
 }
